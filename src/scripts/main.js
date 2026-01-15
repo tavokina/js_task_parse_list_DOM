dbedc7d59b09e7fe522be2db2f52dbc0f10bfb44
Anna Rodina <tavoshka@gmail.com>
2026-01-15 21:18:11 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+
+function sortList() {
+  const employees = [...list.children];
+
+  employees.sort(
+    (a, b) =>
+      Number(b.dataset.salary.replace('$', '')) -
+      Number(a.dataset.salary.replace('$', '')),
+  );
+
+  employees.forEach((employee) => list.append(employee));
+}
+
+function getEmployees() {
+  return [...list.children].map((employee) => ({
+    name: employee.textContent.trim(),
+    position: employee.dataset.position,
+    salary: Number(employee.dataset.salary.replace('$', '')),
+    age: employee.dataset.age,
+  }));
+}
+
+sortList(list);
+getEmployees(list);
